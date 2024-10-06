fab709084e76e5ee30dc09088db8fa730d98c5b6
José Ronaldo <dev.joseronaldo@gmail.com>
2024-10-06 02:07:44 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Habilitar CORS para todos os hosts
+  app.use(cors());
+
+  // Ou, se preferir, use enableCors com configurações específicas
+  app.enableCors({
+    origin: '*', // Permite todos os hosts
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Authorization', // Cabeçalhos permitidos
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
